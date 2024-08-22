612857e8a0a437c05e92d5d639a23c00b72586b7
ductrong6104 <trongd2002@gmail.com>
2024-08-22 14:45:25 +0700

    Initital empty commit

--- src/main/java/org/example/dienluc/repository/StatisticalRepository.java
+++
@@ -1,9 +0,0 @@
-package org.example.dienluc.repository;
-
-import org.example.dienluc.entity.Contract;
-import org.example.dienluc.service.dto.statistical.consumption.StatisticalComsumptionElectricTypeDto;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import java.util.List;
-
--- src/main/java/org/example/dienluc/service/RoleService.java
+++
@@ -1,7 +0,0 @@
-package org.example.dienluc.service;
-
-import org.example.dienluc.entity.Role;
-
-public interface RoleService {
-    public Role getRoleById(Integer roleId);
-}
